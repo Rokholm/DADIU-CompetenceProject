f826c69111f4f778b58292eb21d2bf1ca088a0f5
Kristian Rokholm <krje13@student.aau.dk>
2017-10-15 23:57:28 +0200

    Made so that instance of prefab is created when space is pressed.

---
+++ Competence project - BlackJack/Assets/Scripts/DrawCard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DrawCard : MonoBehaviour {
+
+	public Transform card;
+	
+	
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			Instantiate(card, new Vector3(0, 0, -1), Quaternion.identity);
+		}
+
+	
+	}
+}
