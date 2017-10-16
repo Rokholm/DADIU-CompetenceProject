53dd48b00f0e613b3d3e8225a11dbbc27bb08891
Kristian Rokholm <krje13@student.aau.dk>
2017-10-16 13:55:18 +0200

    Created prefab and script that spawns cards

---
+++ Competence project - BlackJack/Assets/Scripts/DrawCard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCard : MonoBehaviour {
+	[SerializeField, Tooltip("Defines the card prefab that should be spawned")]
+	public GameObject card;
+	private Renderer rend;
+
+	void Start()
+	{
+		rend = card.GetComponent<Renderer>();
+	}
+
+	void Update()
+	{
+
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			//Debug.Log("Space key pressed");
+			SpawnCard();
+		}
+	}
+
+	void SpawnCard()
+	{
+		Instantiate(card, new Vector3(DisplaceCards(), 0.0f, 0.0f), transform.rotation);
+	}
+
+	float DisplaceCards()
+	{
+		//Vector3 center = rend.bounds.center;
+		float radius = rend.bounds.extents.magnitude;
+		float displaceAmount = +radius * 0.5f;
+
+		return displaceAmount;
+	}
+}
